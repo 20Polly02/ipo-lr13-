fb8359987bbc161a897072f4581a989e8c586b2b
Polly Bel <polialekseevna80@gmail.com>
2024-12-17 23:17:02 +0300

    Delete collision directory

--- collision/isCorrectRect.py
+++
@@ -1,9 +0,0 @@
-def isCorrectRect():
-    left_lower= (-7, 9)  # Координаты первой точки
-    right_upper= (3, 6)   # Координаты второй точки
-
-    # Проверяем условия корректности
-    if left_lower[0] > right_upper[0] or left_lower[1] > right_upper[1]:
-        print("False")  # Некорректный прямоугольник
-    else:
-        print("True")   # Корректный прямоугольник
